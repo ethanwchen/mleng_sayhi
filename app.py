fc192739d9d1ea1ae63554b4cfe82b302f317af6
ethanwchen <96222805+ethanwchen@users.noreply.github.com>
2025-03-09 17:29:51 -0400

    pylint 8.82/10

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+"""Main application script for generating a greeting file."""
+
 import os
 from datetime import datetime
 
@@ -7,6 +9,7 @@ VERSION = "1.0.0"
 DEFAULT_DIR = "/app/data/"
 
 def say_hi(msg: str = "Hello from CI/CD deployment!") -> None:
+    """Creates a file with a greeting message."""
     # Generate timestamp
     timestamp = datetime.now().strftime("%Y%m%d%H%M")
 
@@ -18,16 +21,14 @@ def say_hi(msg: str = "Hello from CI/CD deployment!") -> None:
     file_path = os.path.join(DEFAULT_DIR, file_name)
 
     # Write message to the file
-    with open(file_path, "w") as file:
+    with open(file_path, "w", encoding="utf-8") as file:
         file.write(msg)
 
     print(f"File '{file_path}' created successfully.")
 
-def add_numbers(a: int, b: int) -> int:
-    """
-    Function to add two numbers and return the result.
-    """
-    return a + b
+def add_numbers(first_number: int, second_number: int) -> int:
+    """Returns the sum of two numbers."""
+    return first_number + second_number
 
 if __name__ == "__main__":
     say_hi()
